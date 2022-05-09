297e617a0502d40817b89187a84d8febd01e8363
Abinash Dash <abinashdash17@gmail.com>
2022-05-10 02:16:55 +0530

    create a static page server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, './static')));
+
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`express listening on port${port}`);
+});
